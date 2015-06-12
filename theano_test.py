7c05b497e9cf94b60059b6ad77a436e3b636cf0e
cclamb <chrislambistan@gmail.com>
2015-06-12 17:33:10 -0600

    some work

--- theano_test.py
+++ theano_test.py
@@ -1,4 +1,30 @@
 __author__ = 'cclamb'
 
 
+import numpy as np
+import theano.tensor as T
+
+from theano import function
+
+def cell():
+    value = T.scalar('value')
+    weight = T.scalar('weight')
+    transform = T.dmatrix('transform')
+    return function([value, weight, transform], value * weight * transform)
+
+class RouterCell(object):
+
+    def __init__(self, weight=1.0, transformation=np.array([[0, 1], [0, 0]])):
+        return
+
+class Router(object):
+
+    def __init__(self):
+        return
+
+class Processor(object):
+
+    def __init__(self):
+        return
+
 
