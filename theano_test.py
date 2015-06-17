83b0bd4b81bf40c9e1a94ea9a21e02224dfba561
cclamb <cclamb@sandia.gov>
2015-06-17 14:17:50 -0600

    prototypical router and memory

--- theano_test.py
+++ theano_test.py
@@ -7,19 +7,54 @@ import theano.tensor as T
 from theano import function
 
 
-def cell():
-    value = T.scalar('value')
-    weight = T.scalar('weight')
-    transform = T.dmatrix('transform')
-    return function([value, weight, transform], value * weight * transform)
-
-
 class Router(object):
 
     def __init__(self):
         self.values = [
-            [(1, np.array([[1, 0], [0, 0]])), (2, np.array([[0, 1], [0, 1]]))],
-            [(2.3, np.array([[0, 0], [1, 0]])), (3.2, np.array([[0, 1], [0, 0]]))]
+            [
+                (1, np.array
+                    (
+                        [
+                            [[1, 0],
+                             [0, 0]],
+                            [[0, 0],
+                             [0, 1]]
+                        ]
+                    )
+                 ),
+                (2, np.array
+                    (
+                        [
+                            [[1, 0],
+                             [0, 0]],
+                            [[0, 1],
+                             [0, 0]]
+                        ]
+                    )
+                )
+            ],
+            [
+                (2.3, np.array
+                    (
+                        [
+                            [[0, 0],
+                             [0, 1]],
+                            [[1, 0],
+                             [0, 0]]
+                        ]
+                    )
+                ),
+                (3.2, np.array
+                    (
+                        [
+                            [[0, 0],
+                             [0, 0]],
+                            [[0, 1],
+                             [0, 0]]
+                        ]
+                    )
+                ),
+            ]
         ]
         value = T.scalar('value')
         weight = T.scalar('weight')
@@ -31,20 +66,39 @@ class Router(object):
         result = [[0, 0], [0, 0]]
         for i in range(2):
             for j in range(2):
-                result[i][j] = self.mapping(values[i][j], self.values[i][j][0], self.values[i][j][1])
+                # import ipdb
+                # ipdb.set_trace()
+                value = values[i][j]
+                weight = self.values[i][j][0]
+                transform = self.values[i][j][1]
+                result[i][j] = self.mapping(
+                    value,
+                    weight,
+                    transform.reshape(2, 4)
+                ).reshape(2, 2, 2)
         return result
 
 
 class Memory(object):
 
     def __init__(self):
-        self.memory = np.array([[0, 0], [0, 0]])
+        self.memory = np.array(
+            [
+                [[0, 0],
+                 [0, 0]],
+                [[0, 0],
+                 [0, 0]]
+            ]
+        )
         memory = T.dmatrix('memory')
         value = T.dmatrix('value')
         self.mapping = function([memory, value], memory + value)
 
     def insert(self, value):
-        self.memory = self.mapping(value, self.memory)
+        self.memory = self.mapping(
+            value.reshape(2, 4),
+            self.memory.reshape(2, 4)
+        ).reshape(2, 2, 2)
 
 
 class Pipeline(object):
